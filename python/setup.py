b4cd7a4405a7343b746c77b81abbb2385de740ae
gcca <cristhian@blazingdb.com>
2018-09-26 14:19:57 -0500

    [connection] Add setup tool

---
+++ python/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+
+setup(name='blazingdb-protocol',
+      version='1.0',
+      description='Messaging system for BlazingDB',
+      author='BlazingDB Team',
+      author_email='blazing@blazingdb',
+      url='https://github.com/BlazingDB/blazingdb-protocol',
+      packages=['blazingdb.protocol'],
+)
