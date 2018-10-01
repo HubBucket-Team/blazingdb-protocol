6a08742bc4d92116e95a6224ca401fd8cbe4b4a1
gcca <cristhian@blazingdb.com>
2018-10-01 15:21:45 -0500

    [error-handling] Add first error types

---
+++ java/src/main/java/com/blazingdb/protocol/calcite/errors/SyntaxError.java
@@ -0,0 +1,5 @@
+package com.blazingdb.protocol.calcite.errors;
+
+import com.blazingdb.protocol.errors.Error;
+
+public class SyntaxError extends Error {}
---
+++ java/src/main/java/com/blazingdb/protocol/interpreter/errors/TokenCreationError.java
@@ -0,0 +1,5 @@
+package com.blazingdb.protocol.interpreter.errors;
+
+import com.blazingdb.protocol.errors.Error;
+
+public class TokenCreationError extends Error {}
