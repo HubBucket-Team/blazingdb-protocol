46d1c6ec129582dab6c64961dfce141f61478afa
gcca <cristhian@blazingdb.com>
2018-10-02 17:02:19 -0500

    Add response and errors schemas

--- python/blazingdb/protocol/transport/channel.py
+++ python/blazingdb/protocol/transport/channel.py
@@ -2,12 +2,23 @@ import flatbuffers
 
 import blazingdb.protocol.transport as transport
 
-from blazingdb.messages.blazingdb.protocol import Header, Request, Response
+from blazingdb.messages.blazingdb.protocol.Status import Status
+from blazingdb.messages.blazingdb.protocol \
+  import Header, Request, Response, ResponseError
 
 
 class RequestSchema(transport.schema(Request)):
   header = transport.StructSegment(Header)
   payload = transport.BytesSegment()
+
+
+class ResponseSchema(transport.schema(Response)):
+  status = transport.NumberSegment()  # todo(gcca): [Enum,Choice]Segment
+  payload = transport.BytesSegment()
+
+
+class ResponseErrorSchema(transport.schema(ResponseError)):
+  errors = transport.StringSegment()
 
 
 def MakeRequestBuffer(messageType, accessToken, schema, builderInitialSize=0):
