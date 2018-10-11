de517e67ab8f4968b42408546c78ec4830f95269
Alexander <aocsa.cs@gmail.com>
2018-10-11 15:58:19 -0500

    Auto stash before merge of "develop" and "origin/develop"

--- python/blazingdb/messages/blazingdb/protocol/orchestrator/DMLRequest.py
+++ python/blazingdb/messages/blazingdb/protocol/orchestrator/DMLRequest.py
@@ -30,7 +30,7 @@ class DMLRequest(object):
         o = flatbuffers.number_types.UOffsetTFlags.py_type(self._tab.Offset(6))
         if o != 0:
             x = self._tab.Indirect(o + self._tab.Pos)
-            from .TableGroup import TableGroup
+            from blazingdb.messages.blazingdb.protocol.TableGroup import TableGroup
             obj = TableGroup()
             obj.Init(self._tab.Bytes, x)
             return obj
