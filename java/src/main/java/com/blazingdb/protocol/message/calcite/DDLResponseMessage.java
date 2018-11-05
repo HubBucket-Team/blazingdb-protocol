5df746f028d8bba74cf10f82a18f50058bda628e
gcca <cristhian@blazingdb.com>
2018-11-05 15:29:12 -0500

    [timing] Add time parameter to calcite DDL and DML

--- java/src/main/java/com/blazingdb/protocol/message/calcite/DDLResponseMessage.java
+++ java/src/main/java/com/blazingdb/protocol/message/calcite/DDLResponseMessage.java
@@ -1,21 +1,32 @@
 package com.blazingdb.protocol.message.calcite;
 
+import blazingdb.protocol.calcite.DDLResponse;
+
 import com.blazingdb.protocol.message.IMessage;
 import com.blazingdb.protocol.util.ByteBufferUtil;
+
+import com.google.flatbuffers.FlatBufferBuilder;
 
 import java.nio.ByteBuffer;
 
 public class DDLResponseMessage implements IMessage {
+		private final long time;
+
     public DDLResponseMessage(ByteBuffer payload) {
+				DDLResponse message = DDLResponse.getRootAsDDLResponse(payload);
+				this.time = message.time();
     }
 
-    public DDLResponseMessage() {
-
+    public DDLResponseMessage(final long time) {
+				this.time = time;
     }
 
     @Override
     public ByteBuffer getBufferData() {
-        return ByteBufferUtil.addEof(ByteBuffer.wrap("".getBytes()));
+        FlatBufferBuilder builder = new FlatBufferBuilder(1024);
+        int root = DDLResponse.createDDLResponse(builder, time);
+        builder.finish(root);
+        return ByteBufferUtil.addEof(builder.dataBuffer());
     }
     @Override
     public long getBufferSize() {
