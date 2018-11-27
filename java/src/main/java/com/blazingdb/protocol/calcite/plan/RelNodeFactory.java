565745afdca13421c76d47267f75197747f3730b
gcca <cristhian@blazingdb.com>
2018-11-27 18:10:22 -0500

    [logical-plan] Test table scan

--- java/src/main/java/com/blazingdb/protocol/calcite/plan/RelNodeFactory.java
+++ java/src/main/java/com/blazingdb/protocol/calcite/plan/RelNodeFactory.java
@@ -1,67 +1,109 @@
 package com.blazingdb.protocol.calcite.plan;
 
-import com.blazingdb.protocol.calcite.plan.messages.Node;
-import com.blazingdb.protocol.calcite.plan.messages.NodeType;
-import com.blazingdb.protocol.calcite.plan.messages.TableScanData;
-import com.blazingdb.protocol.calcite.plan.messages.UnionData;
+import com.blazingdb.protocol.calcite.plan.messages.RelNode;
+import com.blazingdb.protocol.calcite.plan.messages.RelNodeType;
+import com.blazingdb.protocol.calcite.plan.messages.TableScan;
 
 import com.google.flatbuffers.FlatBufferBuilder;
 
 import org.apache.commons.lang3.NotImplementedException;
 import org.apache.commons.lang3.Validate;
 
-import java.util.Collection;
-import java.util.List;
-
 import java.nio.ByteBuffer;
 
-final class NodeFactory {
+import java.util.Collection;
+
+final class RelNodeFactory {
 
   private final FlatBufferBuilder flatBufferBuilder;
 
   // TODO(gcca): test `ContextFBObjectsPool` (on building)
-  public NodeFactory(final FlatBufferBuilder flatBufferBuilder) {
+  public RelNodeFactory(final FlatBufferBuilder flatBufferBuilder) {
     Validate.notNull(flatBufferBuilder, "FlatBufferBuilder is required");
     this.flatBufferBuilder = flatBufferBuilder;
   }
 
   // TODO(gcca): abstract node factory
-  public NodeFactory() { this.flatBufferBuilder = new FlatBufferBuilder(0); }
-
-  public NodeFactory(Integer initialSize) {
-    this.flatBufferBuilder = new FlatBufferBuilder(initialSize);
+  public RelNodeFactory(Integer initialSize) {
+    this(new FlatBufferBuilder(initialSize));
   }
 
-  public Node createNode(final Collection<Node> children) {
+  // TODO(gcca): idem
+  public RelNodeFactory() { this(0); }
+
+  public RelNode createNode(final Collection<RelNode> children) {
     throw new NotImplementedException("Build from messages");
+  }
+
+  public void finish(final Integer rootNodeOffset) {
+    flatBufferBuilder.finish(rootNodeOffset);
   }
 
   public ByteBuffer getDataBuffer() { return flatBufferBuilder.dataBuffer(); }
 
   public byte[] makeDataBuffer() { return flatBufferBuilder.sizedByteArray(); }
 
-  public Integer createTableScanOffset(final List<String> qualifiedName) {
-    final Integer qualifiedNameOffset = TableScanData.createQualifiedNameVector(
-        flatBufferBuilder,
-        qualifiedName.stream()
-            .mapToInt(flatBufferBuilder::createString)
-            .toArray());
-    final Integer tableScanDataOffset = TableScanData.createTableScanData(
-        flatBufferBuilder, qualifiedNameOffset);
-    return createNodeOffsetWithoutChildren(NodeType.TableScan,
-                                           tableScanDataOffset);
+  public Integer createRootRelNodeOffset(final int... inputOffsets) {
+    final Integer inputsOffset =
+        RelNode.createInputsVector(flatBufferBuilder, inputOffsets);
+    RelNode.startRelNode(flatBufferBuilder);
+    RelNode.addInputs(flatBufferBuilder, inputsOffset);
+    RelNode.addType(flatBufferBuilder, RelNodeType.Root);
+    return RelNode.endRelNode(flatBufferBuilder);
   }
 
-  public Integer createUnionOffset(final boolean all,
-                                   final Integer leftOffset,
-                                   final Integer rightOffset) {
-    final Integer unionDataOffset =
-        UnionData.createUnionData(flatBufferBuilder, all);
-    return createNodeOffsetWithoutChildren(NodeType.Union, unionDataOffset);
+  public Integer createTableScanNodeOffset() {
+    FlatBufferBuilder localFlatBufferBuilder = new FlatBufferBuilder(0);
+
+    int[] ss = new int[2];
+    ss[0]    = localFlatBufferBuilder.createString("LOCAL");
+    ss[1]    = localFlatBufferBuilder.createString("THREAD");
+
+    final Integer qualifiedNameOffset =
+        TableScan.createQualifiedNameVector(localFlatBufferBuilder, ss);
+
+    final Integer tableScanOffset =
+        TableScan.createTableScan(localFlatBufferBuilder, qualifiedNameOffset);
+
+    localFlatBufferBuilder.finish(tableScanOffset);
+    final byte[] tableScanBytes = localFlatBufferBuilder.sizedByteArray();
+    localFlatBufferBuilder      = null;
+
+    return createRelNodeOffset(RelNodeType.TableScan, tableScanBytes);
   }
 
-  protected Integer createNodeOffsetWithoutChildren(final byte nodeType,
-                                                    final Integer dataOffset) {
-    return Node.createNode(flatBufferBuilder, nodeType, dataOffset, 0);
+  protected Integer createRelNodeOffset(final short relNodeType,
+                                        final       byte[] data,
+                                        final int... inputOffsets) {
+    final FlatBufferBuilder localFlatBufferBuilder = new FlatBufferBuilder(0);
+    final int               dataOffset =
+        RelNode.createDataVector(localFlatBufferBuilder, data);
+    return createRelNodeOffset(
+        localFlatBufferBuilder, relNodeType, dataOffset, inputOffsets);
+  }
+
+  protected Integer
+  createRelNodeOffset(final FlatBufferBuilder localFlatBufferBuilder,
+                      final short             relNodeType,
+                      final Integer dataOffset,
+                      final int... inputOffsets) {
+    Integer relNodeOffset;
+
+    if (0 == inputOffsets.length) {
+      RelNode.startRelNode(localFlatBufferBuilder);
+      RelNode.addData(localFlatBufferBuilder, dataOffset);
+      RelNode.addType(localFlatBufferBuilder, relNodeType);
+      relNodeOffset = RelNode.endRelNode(localFlatBufferBuilder);
+    } else {
+      final Integer inputsOffset =
+          RelNode.createInputsVector(localFlatBufferBuilder, inputOffsets);
+      relNodeOffset = RelNode.createRelNode(
+          localFlatBufferBuilder, relNodeType, dataOffset, inputsOffset);
+    }
+
+    localFlatBufferBuilder.finish(relNodeOffset);
+    final byte[] relNodeBytes = localFlatBufferBuilder.sizedByteArray();
+
+    return flatBufferBuilder.createByteVector(relNodeBytes);
   }
 }
--- java/src/test/java/com/blazingdb/protocol/calcite/plan/NodeCreationTest.java
+++ java/src/test/java/com/blazingdb/protocol/calcite/plan/NodeCreationTest.java
@@ -1,11 +1,55 @@
 package com.blazingdb.protocol.calcite.plan;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertTrue;
+
+import com.blazingdb.protocol.calcite.plan.RelNodeFactory;
+import com.blazingdb.protocol.calcite.plan.messages.RelNode;
+import com.blazingdb.protocol.calcite.plan.messages.RelNodeType;
+import com.blazingdb.protocol.calcite.plan.messages.TableScan;
+
 import org.junit.Test;
+
+import java.nio.ByteBuffer;
+
+import java.util.Arrays;
+import java.util.List;
 
 public final class NodeCreationTest {
 
   @Test
   public void testCreation() {
-    NodeFactory builder = new NodeFactory();
+    RelNodeFactory relNodeFactory = new RelNodeFactory();
+
+    final List<String> leftQualifiedName  = Arrays.asList("left", "table");
+    final List<String> rightQualifiedName = Arrays.asList("right", "table");
+
+    final Integer leftTableScanRelNodeOffset =
+        relNodeFactory.createTableScanNodeOffset();
+
+    final Integer rootRelNodeOffset =
+        relNodeFactory.createRootRelNodeOffset(leftTableScanRelNodeOffset);
+
+    relNodeFactory.finish(rootRelNodeOffset);
+    final byte[] rootRelNodeBytes = relNodeFactory.makeDataBuffer();
+    final RelNode rootRelNode =
+        RelNode.getRootAsRelNode(ByteBuffer.wrap(rootRelNodeBytes));
+
+    assertEquals(RelNodeType.Root, rootRelNode.type());
+    assertEquals(0, rootRelNode.dataLength());
+    assertEquals(1, rootRelNode.inputsLength());
+
+    final RelNode leftTableScanRelNode = rootRelNode.inputs(0);
+    assertNotEquals(0, leftTableScanRelNode.dataLength());
+    assertEquals(RelNodeType.TableScan, leftTableScanRelNode.type());
+    assertEquals(0, leftTableScanRelNode.inputsLength());
+
+    final byte[] leftTableScanBytes =
+        leftTableScanRelNode.dataAsByteBuffer().array();
+    final TableScan leftTableScan =
+        TableScan.getRootAsTableScan(ByteBuffer.wrap(leftTableScanBytes));
+    assertEquals(2, leftTableScan.qualifiedNameLength());
   }
 }
