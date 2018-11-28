f81fb92b561978511f2dd7113b3846f19ee1ed04
gcca <cristhian@blazingdb.com>
2018-11-28 01:28:25 -0500

    [logical-plan] Create input ref rex nodes

--- java/src/main/java/com/blazingdb/protocol/calcite/plan/RexNodeFactory.java
+++ java/src/main/java/com/blazingdb/protocol/calcite/plan/RexNodeFactory.java
@@ -1,5 +1,6 @@
 package com.blazingdb.protocol.calcite.plan;
 
+import com.blazingdb.protocol.calcite.plan.messages.RexInputRef;
 import com.blazingdb.protocol.calcite.plan.messages.RexNode;
 import com.blazingdb.protocol.calcite.plan.messages.RexNodeType;
 
@@ -8,6 +9,7 @@ import org.apache.commons.lang3.Validate;
 import com.google.flatbuffers.FlatBufferBuilder;
 
 import java.util.Collection;
+import java.util.Optional;
 
 final class RexNodeFactory {
 
@@ -25,20 +27,71 @@ final class RexNodeFactory {
                                0);
   }
 
-  protected Integer createRexNodeOffset(final short rexNodeType,
-                                        final short sqlKind,
-                                        final short sqlTypeName,
-                                        final       byte[] data) {
-    final Integer dataOffset =
-        RexNode.createDataVector(flatBufferBuilder, data);
-    return createRexNodeOffset(rexNodeType, sqlKind, sqlTypeName, dataOffset);
+  public Integer
+  createRexInputRefNodeOffset(final SqlKind sqlKind,
+                              final Collection<Integer> indices) {
+    FlatBufferBuilder localFlatBufferBuilder = new FlatBufferBuilder(0);
+    int[] indexData = indices.stream().mapToInt(Integer::intValue).toArray();
+    final Integer indexOffset =
+        RexInputRef.createIndexVector(localFlatBufferBuilder, indexData);
+    final Integer rexInputRefOffset =
+        RexInputRef.createRexInputRef(localFlatBufferBuilder, indexOffset);
+    localFlatBufferBuilder.finish(rexInputRefOffset);
+    final byte[] rexInputRefBytes = localFlatBufferBuilder.sizedByteArray();
+    localFlatBufferBuilder        = null;
+    return createRexNodeOffset(
+        RexNodeType.InputRef, (short) sqlKind.ordinal(), rexInputRefBytes);
   }
 
-  protected Integer createRexNodeOffset(final short rexNodeType,
-                                        final short sqlKind,
-                                        final short sqlTypeName,
+  protected Integer createRexNodeOffset(final Short rexNodeType,
+                                        final Short sqlKind,
+                                        final Short sqlTypeName,
+                                        final       byte[] data) {
+    return createRexNodeOffset(
+        rexNodeType, sqlKind, Optional.of(sqlTypeName), data);
+  }
+
+  protected Integer createRexNodeOffset(final Short rexNodeType,
+                                        final Short sqlKind,
+                                        final       byte[] data) {
+    return createRexNodeOffset(rexNodeType, sqlKind, Optional.empty(), data);
+  }
+
+  protected Integer createRexNodeOffset(final Short rexNodeType,
+                                        final Short sqlKind,
+                                        final Short sqlTypeName,
                                         final Integer dataOffset) {
-    return RexNode.createRexNode(
-        flatBufferBuilder, rexNodeType, sqlKind, sqlTypeName, dataOffset);
+    return createRexNodeOffset(
+        rexNodeType, sqlKind, Optional.of(sqlTypeName), dataOffset);
+  }
+
+  protected Integer createRexNodeOffset(final Short rexNodeType,
+                                        final Short sqlKind,
+                                        final Integer dataOffset) {
+    return createRexNodeOffset(
+        rexNodeType, sqlKind, Optional.empty(), dataOffset);
+  }
+
+  protected Integer
+  createRexNodeOffset(final Short rexNodeType,
+                      final Short sqlKind,
+                      final Optional<Short> sqlTypeNameOptional,
+                      final                 byte[] data) {
+    final Integer dataOffset =
+        RexNode.createDataVector(flatBufferBuilder, data);
+    return createRexNodeOffset(
+        rexNodeType, sqlKind, sqlTypeNameOptional, dataOffset);
+  }
+
+  protected Integer
+  createRexNodeOffset(final Short rexNodeType,
+                      final Short sqlKind,
+                      final Optional<Short> sqlTypeNameOptional,
+                      final Integer dataOffset) {
+    return RexNode.createRexNode(flatBufferBuilder,
+                                 rexNodeType,
+                                 sqlKind,
+                                 sqlTypeNameOptional.orElse((short) -1),
+                                 dataOffset);
   }
 }
