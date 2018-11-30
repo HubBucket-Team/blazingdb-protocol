d78c47da9f340407fa407abf0357dd9d5ee0a77c
Christian Noboa Mardini <christian@blazingdb.com>
2018-11-30 17:59:29 -0500

    [logical-plan] Added the tests for rel visitor and rex visitor.

---
+++ cpp/tests/calcite/dto/MockRelVisitor.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "gmock/gmock.h"
+#include "rel/visitor/RelVisitor.h"
+
+namespace LogicalPlan = blazingdb::protocol::dto;
+
+struct MockRelVisitor : public LogicalPlan::RelVisitor {
+    MOCK_METHOD1(visit, void(LogicalPlan::LogicalAggregate*));
+
+    MOCK_METHOD1(visit, void(LogicalPlan::LogicalFilter*));
+
+    MOCK_METHOD1(visit, void(LogicalPlan::LogicalProject*));
+
+    MOCK_METHOD1(visit, void(LogicalPlan::LogicalUnion*));
+
+    MOCK_METHOD1(visit, void(LogicalPlan::TableScan*));
+};
