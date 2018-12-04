67b41d3db5b5bb02d66427d2e5bbc96107121467
gcca <cristhian@blazingdb.com>
2018-12-04 12:00:38 -0500

    [fix-column-size-gdfcolumn] Fix gdf column handler size type to ulong

--- python/blazingdb/messages/blazingdb/protocol/orchestrator/DDLDropTableRequest.py
+++ python/blazingdb/messages/blazingdb/protocol/orchestrator/DDLDropTableRequest.py
@@ -23,14 +23,14 @@ class DDLDropTableRequest(object):
         o = flatbuffers.number_types.UOffsetTFlags.py_type(self._tab.Offset(4))
         if o != 0:
             return self._tab.String(o + self._tab.Pos)
-        return None
+        return bytes()
 
     # DDLDropTableRequest
     def DbName(self):
         o = flatbuffers.number_types.UOffsetTFlags.py_type(self._tab.Offset(6))
         if o != 0:
             return self._tab.String(o + self._tab.Pos)
-        return None
+        return bytes()
 
 def DDLDropTableRequestStart(builder): builder.StartObject(2)
 def DDLDropTableRequestAddName(builder, name): builder.PrependUOffsetTRelativeSlot(0, flatbuffers.number_types.UOffsetTFlags.py_type(name), 0)
