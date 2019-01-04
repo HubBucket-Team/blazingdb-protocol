5db6d6bcffcdadfdf1d817f9ee5397505606566b
Alexander <aocsa.cs@gmail.com>
2019-01-04 18:19:27 -0500

    [zeromq] add send method to ZeroMqClient

--- cpp/src/blazingdb/protocol/client/client.h
+++ cpp/src/blazingdb/protocol/client/client.h
@@ -27,9 +27,21 @@ public:
   explicit ZeroMqClient(const std::string &connection): context{zmq_ctx_new()}, socket{ zmq_socket (context, ZMQ_REQ) } {
     auto rc = zmq_connect(socket, connection.c_str());
     //assert (rc == 0);
- }
-  
- Buffer send(const Buffer &bufferedData) {
+  } 
+
+  Buffer send(std::shared_ptr<flatbuffers::DetachedBuffer> &bufferedData) {
+    zmq_send(socket, bufferedData->data(), bufferedData->size(), 0);
+    zmq_msg_t msg;
+    int rc = zmq_msg_init(&msg);
+    //assert(rc != 0);
+    zmq_msg_recv(&msg, socket, 0);
+    auto size = zmq_msg_size(&msg);
+    Buffer responseBuffer((uint8_t*) zmq_msg_data(&msg), size);
+   zmq_msg_close (&msg);
+    return responseBuffer;
+  }
+
+  Buffer send(const Buffer &bufferedData) {
     zmq_send(socket, bufferedData.data(), bufferedData.size(), 0);
     zmq_msg_t msg;
     int rc = zmq_msg_init(&msg);
