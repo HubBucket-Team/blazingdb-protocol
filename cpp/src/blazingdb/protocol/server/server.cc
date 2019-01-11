af029cef2ba937ac9d65dab406fec4b66b68d231
gcca <cristhian@blazingdb.com>
2019-01-10 23:30:52 -0500

    [feature/tcp] Remove threading lock

--- cpp/src/blazingdb/protocol/server/server.cc
+++ cpp/src/blazingdb/protocol/server/server.cc
@@ -27,36 +27,30 @@ Server::Server(const Connection &connection) : connection_(connection) {
 
 void Server::_Start(const __HandlerBaseType &handler) const {
   for (;;) {
-    std::thread thread ([&]() {
-      int fd = accept4(connection_.fd(), nullptr, nullptr, SOCK_CLOEXEC);
-      if (fd == -1) { throw std::runtime_error("accept error"); }
-      uint32_t length;
-      ssize_t nread = read(fd, (void*)&length, sizeof(uint32_t));
-      while (nread > 0) {
-        std::uint8_t buffer[length];
-        nread = read(fd, buffer, length);
-        auto responseBuffer =
-            handler->call(Buffer(buffer, static_cast<std::size_t>(nread)));
-        uint32_t responseBufferLength = responseBuffer.size();
-        ssize_t written_bytes =
-            write(fd, (void*)&responseBufferLength, sizeof(uint32_t));
-        written_bytes =
-            write(fd, responseBuffer.data(), responseBuffer.size());
-        if (static_cast<std::size_t>(written_bytes) != responseBuffer.size()) {
-          throw std::runtime_error("write error");
-        }
-        nread = read(fd, (void*)&length, sizeof(uint32_t));
-      }
-      if (nread == -1) {
-        throw std::runtime_error("error read");
-      }
-      if (nread == 0) {
-        close(fd);
-      } else {
-        throw std::runtime_error("unreachable");
-      }
-    });
-    thread.join();
+    int fd = accept4(connection_.fd(), nullptr, nullptr, SOCK_CLOEXEC);
+
+    if (fd == -1) { throw std::runtime_error("accept error"); }
+
+    uint32_t length;
+    ssize_t  nread = read(fd, (void *) &length, sizeof(uint32_t));
+
+    std::uint8_t buffer[length];
+    nread = read(fd, buffer, length);
+    auto responseBuffer =
+        handler->call(Buffer(buffer, static_cast<std::size_t>(nread)));
+
+    uint32_t responseBufferLength = responseBuffer.size();
+
+    ssize_t written_bytes =
+        write(fd, (void *) &responseBufferLength, sizeof(uint32_t));
+    written_bytes = write(fd, responseBuffer.data(), responseBuffer.size());
+
+    if (static_cast<std::size_t>(written_bytes) != responseBuffer.size()) {
+      throw std::runtime_error("write error");
+    }
+
+    if (nread == -1) { throw std::runtime_error("error read"); }
+    close(fd);
   }
 }
 
