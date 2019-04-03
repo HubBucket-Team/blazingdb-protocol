82bb3101a54af1828dc9e505866f2286330f61a6
Rommel <rommel@blazingdb.com>
2019-04-03 12:13:23 -0500

    [custrings-support] Updating remaining APIs with the new custrings fields

--- python/blazingdb/protocol/gdf/__init__.py
+++ python/blazingdb/protocol/gdf/__init__.py
@@ -6,16 +6,17 @@ from blazingdb.messages.blazingdb.protocol.gdf \
 class cudaIpcMemHandle_tSchema(transport.schema(cudaIpcMemHandle_t)):
   reserved = transport.BytesSegment()
 
-
 class gdf_dtype_extra_infoSchema(transport.schema(gdf_dtype_extra_info)):
   time_unit = transport.NumberSegment()
-
+  custrings_views = transport.SchemaSegment(cudaIpcMemHandle_tSchema)
+  custrings_views_count = transport.NumberSegment()
+  custrings_membuffer = transport.SchemaSegment(cudaIpcMemHandle_tSchema)
+  custrings_membuffer_size = transport.NumberSegment()
+  custrings_base_ptr = transport.NumberSegment()
 
 class gdf_columnSchema(transport.schema(gdf_column_handler)):
   data = transport.SchemaSegment(cudaIpcMemHandle_tSchema)
   valid = transport.SchemaSegment(cudaIpcMemHandle_tSchema)
-  custrings_membuffer = transport.SchemaSegment(cudaIpcMemHandle_tSchema)
-  custrings_views = transport.SchemaSegment(cudaIpcMemHandle_tSchema)
   size = transport.NumberSegment()
   dtype = transport.NumberSegment()
   dtype_info = transport.SchemaSegment(gdf_dtype_extra_infoSchema)
