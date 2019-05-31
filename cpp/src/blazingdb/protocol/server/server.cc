a04771408d41d88dd5a181620b0a437978370594
Percy Camilo Triveño Aucahuasi <percy@blazingdb.com>
2019-05-31 12:25:44 -0500

    dont use separate approach for tcp and unix sockets

--- cpp/src/blazingdb/protocol/server/server.cc
+++ cpp/src/blazingdb/protocol/server/server.cc
@@ -25,28 +25,7 @@ Server::Server(const Connection &connection) : connection_(connection) {
   }
 }
 
-// This assumes buffer is at least x bytes long,
-// and that the socket is blocking.
-void ReadXBytes(int socket, uint32_t x, void* buffer)
-{
-    int bytesRead = 0;
-    int result;
-    while (bytesRead < x)
-    {
-        result = read(socket, buffer + bytesRead, x - bytesRead);
-        if (result < 1 )
-        {
-            // Throw your error.
-        }
-
-        bytesRead += result;
-    }
-}
-
 void Server::_Start(const __HandlerBaseType &handler) const {
-
-#ifdef USE_UNIX_SOCKETS
-
   for (;;) {
     int fd = accept4(connection_.fd(), nullptr, nullptr, SOCK_CLOEXEC);
 
@@ -61,42 +40,6 @@ void Server::_Start(const __HandlerBaseType &handler) const {
 
     close(fd);    
   }
-
-#else
-  
-  for (;;) {
-    int fd = accept4(connection_.fd(), nullptr, nullptr, SOCK_CLOEXEC);
-
-    if (fd == -1) { throw std::runtime_error("accept error"); }
-
-    uint32_t length = 0;
-    // we assume that sizeof(length) will return 4 here.
-    ReadXBytes(fd, sizeof(length), (void*)(&length));
-    
-    std::uint8_t buffer[length];
-    ReadXBytes(fd, length, (void*)buffer);
-    
-    auto responseBuffer =
-        handler->call(Buffer(buffer, static_cast<std::size_t>(length)));
-
-    uint32_t responseBufferLength = responseBuffer.size();
-
-    ssize_t written_bytes =
-        write(fd, (void *) &responseBufferLength, sizeof(uint32_t));
-    written_bytes = write(fd, responseBuffer.data(), responseBuffer.size());
-
-    if (static_cast<std::size_t>(written_bytes) != responseBuffer.size()) {
-      throw std::runtime_error("write error");
-    }
-
-    // TODO percy map this error
-    //if (nread == -1) { throw std::runtime_error("error read"); }
-    
-    close(fd);
-  }
-
-#endif
-
 }
 
 
